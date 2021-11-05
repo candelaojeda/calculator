f0850dca72436581a20b333e8c7bd66122d8690b
Candelaojeda <ojedacandelaco@gmail.com>
2021-11-05 15:41:18 -0300

    Initial commit

---
+++ src/components/ButtonPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonPanel = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ButtonPanel
---
+++ src/components/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Display = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Display
